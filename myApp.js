130871c48a8b265d314a7ec960e01c09efd87231
cr440 <72648905+cr440@users.noreply.github.com>
2021-02-04 12:07:17 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,28 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.use(express.static(__dirname + "/public"));
+
+
+// Console.log method path - ip
+app.use(function middleware(req, res, next){
+  var response = req.method + " " + req.path + " - " + req.ip + " - " + req.headers["user-agent"];
+  console.log(response);
+  next();
+})
+
+// app.get("/",function(req, res) {
+//   res.sendFile(__dirname + "/views/index.html");
+// });
+
+app.get("/",function(req, res) {
+  res.send("Hello Express");
+});
 
 
 
